b159f1401cfcff62f3262cbbdb0abe8a11058585
helpfulrobot <chris@silverstripe.com>
2015-12-26 11:22:29 +1300

    Converted to PSR-2

--- code/RootFolder.php
+++ code/RootFolder.php
@@ -9,149 +9,146 @@
  */
 class RootFolder extends DataExtension
 {
-	private static $has_one = array(
-		'RootFolder' => 'Folder',
-	);
+    private static $has_one = array(
+        'RootFolder' => 'Folder',
+    );
 
-	/**
-	 * @var array exclude this page types and class names
-	 */
-	private static $ignored_classes = array('VirtualPage', 'ErrorPage');
+    /**
+     * @var array exclude this page types and class names
+     */
+    private static $ignored_classes = array('VirtualPage', 'ErrorPage');
 
-	/**
-	 * @var bool should folders be created for translated objects?
-	 */
-	private static $create_folder_for_translations = false;
+    /**
+     * @var bool should folders be created for translated objects?
+     */
+    private static $create_folder_for_translations = false;
 
-	/**
-	 * @var string default root for all folders; may be overwritten in config of decorated class
-	 */
-	private static $folder_root = 'Articles';
+    /**
+     * @var string default root for all folders; may be overwritten in config of decorated class
+     */
+    private static $folder_root = 'Articles';
 
-	function onAfterWrite()
-	{
-		if ($this->owner->ID) {
-			$this->checkFolder();
-		}
-	}
+    public function onAfterWrite()
+    {
+        if ($this->owner->ID) {
+            $this->checkFolder();
+        }
+    }
 
-	/**
-	 * Creates a folder for a page as a subfolder of the parent page
-	 * You can exclude page types by setting $ignored_classes in config
-	 *
-	 * Doesn't create folders for translated pages by default.
-	 *
-	 * @TODO doesn't check if page is moved to another parent
-	 */
-	function checkFolder()
-	{
-		$ignoredPageTypes = Config::inst()->get($this->class, 'ignored_classes');
+    /**
+     * Creates a folder for a page as a subfolder of the parent page
+     * You can exclude page types by setting $ignored_classes in config
+     *
+     * Doesn't create folders for translated pages by default.
+     *
+     * @TODO doesn't check if page is moved to another parent
+     */
+    public function checkFolder()
+    {
+        $ignoredPageTypes = Config::inst()->get($this->class, 'ignored_classes');
 
-		foreach ($ignoredPageTypes as $pagetype) {
-			if (is_a($this->owner, $pagetype)) {
-				return;
-			}
-		}
+        foreach ($ignoredPageTypes as $pagetype) {
+            if (is_a($this->owner, $pagetype)) {
+                return;
+            }
+        }
 
-		if (class_exists('Translatable')
-			&& $this->owner->Locale !== Translatable::default_locale()
-			&& !Config::inst()->get($this->class, 'create_folder_for_translations')
-		) {
-			return;
-		}
+        if (class_exists('Translatable')
+            && $this->owner->Locale !== Translatable::default_locale()
+            && !Config::inst()->get($this->class, 'create_folder_for_translations')
+        ) {
+            return;
+        }
 
-		if (!$this->owner->RootFolderID) {
-			$this->createRootFolder();
-		} else {
-			$this->updateRootFolder();
-		}
-	}
+        if (!$this->owner->RootFolderID) {
+            $this->createRootFolder();
+        } else {
+            $this->updateRootFolder();
+        }
+    }
 
-	/**
-	 * Does the work of creating a new RootFolder, saves the relation in the extended DataObject
-	 */
-	protected function createRootFolder()
-	{
-		//get path to parent folder
-		$parent = $this->owner->hasExtension('Hierarchy')
-			? $this->owner->getParent()
-			: null;
-		if (is_a($parent, 'Page') && $parentFolder = $parent->RootFolder()) {
-			$folderRoot = $parent->getRootFolderName();
-		} else {
-			//fallback to classes folder_root which is defined in your config.yml
-			$folderRoot = $this->getFolderRoot() . '/';
-		}
+    /**
+     * Does the work of creating a new RootFolder, saves the relation in the extended DataObject
+     */
+    protected function createRootFolder()
+    {
+        //get path to parent folder
+        $parent = $this->owner->hasExtension('Hierarchy')
+            ? $this->owner->getParent()
+            : null;
+        if (is_a($parent, 'Page') && $parentFolder = $parent->RootFolder()) {
+            $folderRoot = $parent->getRootFolderName();
+        } else {
+            //fallback to classes folder_root which is defined in your config.yml
+            $folderRoot = $this->getFolderRoot() . '/';
+        }
 
-		if ($folderRoot == '/') {
-			$folderRoot = getFolderRoot() . '/';
-		}
+        if ($folderRoot == '/') {
+            $folderRoot = getFolderRoot() . '/';
+        }
 
-		$folder = Folder::find_or_make($folderRoot . $this->owner->URLSegment);
-		$folder->Title = $this->owner->Title;
-		$folder->setName($this->owner->URLSegment);
-		$folder->write();
+        $folder = Folder::find_or_make($folderRoot . $this->owner->URLSegment);
+        $folder->Title = $this->owner->Title;
+        $folder->setName($this->owner->URLSegment);
+        $folder->write();
 
-		$this->owner->RootFolderID = $folder->ID;
-		$this->owner->write();
-	}
+        $this->owner->RootFolderID = $folder->ID;
+        $this->owner->write();
+    }
 
-	/**
-	 * Does the work of updating the folder if the URLSegment or ParentID is changed.
-	 * if both it does two writes...
-	 *
-	 * @todo: rethink moving subfolders as it may timeout on real large trees
-	 */
-	protected function updateRootFolder()
-	{
-		$rootFolder = $this->owner->RootFolder();
-		if ($this->owner->isChanged('URLSegment') && $this->owner->URLSegment) {
-			$rootFolder->setName($this->owner->URLSegment);
-			$rootFolder->write();
-		}
+    /**
+     * Does the work of updating the folder if the URLSegment or ParentID is changed.
+     * if both it does two writes...
+     *
+     * @todo: rethink moving subfolders as it may timeout on real large trees
+     */
+    protected function updateRootFolder()
+    {
+        $rootFolder = $this->owner->RootFolder();
+        if ($this->owner->isChanged('URLSegment') && $this->owner->URLSegment) {
+            $rootFolder->setName($this->owner->URLSegment);
+            $rootFolder->write();
+        }
 
-		if ($this->owner->isChanged('ParentID') && $this->owner->ParentID > 0) {
-			$oldParentID = $rootFolder->ParentID;
-			$newParentID = $this->owner->Parent()->RootFolderID;
-			if ($oldParentID !== $newParentID && $newParentID !== $rootFolder->ID) {
-				$rootFolder->setParentID($newParentID);
-				$rootFolder->write();
-			}
-		}
-	}
+        if ($this->owner->isChanged('ParentID') && $this->owner->ParentID > 0) {
+            $oldParentID = $rootFolder->ParentID;
+            $newParentID = $this->owner->Parent()->RootFolderID;
+            if ($oldParentID !== $newParentID && $newParentID !== $rootFolder->ID) {
+                $rootFolder->setParentID($newParentID);
+                $rootFolder->write();
+            }
+        }
+    }
 
-	/**
-	 * Returns the folder root for the current root folder, e.g. 'Articles',
-	 * if a config $folder_root is defined in the decorated class.
-	 *
-	 * Falls back to global config
-	 */
-	public function getFolderRoot()
-	{
-		return ($this->owner->config()->get('folder_root'))
-			? $this->owner->config()->get('folder_root')
-			: Config::inst()->get($this->class, 'folder_root');
-	}
+    /**
+     * Returns the folder root for the current root folder, e.g. 'Articles',
+     * if a config $folder_root is defined in the decorated class.
+     *
+     * Falls back to global config
+     */
+    public function getFolderRoot()
+    {
+        return ($this->owner->config()->get('folder_root'))
+            ? $this->owner->config()->get('folder_root')
+            : Config::inst()->get($this->class, 'folder_root');
+    }
 
 
-	/**
-	 * Helper function to return the name of the RootFolder for setting in @link UploadField or @link GridFieldBulkUpload
-	 * By default relative to /assets/
-	 *
-	 * @param bool $relativeToAssetsDir
-	 */
-	public function getRootFolderName($relativeToAssetsDir = true)
-	{
-		if ($this->owner->RootFolderID) {
-			return $relativeToAssetsDir
-				? str_replace(ASSETS_DIR . '/', '', $this->owner->RootFolder()->getRelativePath())
-				: $this->owner->RootFolder()->getRelativePath();
-
-		} else {
-			//use folder root as fallback for now
-			return $this->getFolderRoot();
-		}
-
-	}
-
+    /**
+     * Helper function to return the name of the RootFolder for setting in @link UploadField or @link GridFieldBulkUpload
+     * By default relative to /assets/
+     *
+     * @param bool $relativeToAssetsDir
+     */
+    public function getRootFolderName($relativeToAssetsDir = true)
+    {
+        if ($this->owner->RootFolderID) {
+            return $relativeToAssetsDir
+                ? str_replace(ASSETS_DIR . '/', '', $this->owner->RootFolder()->getRelativePath())
+                : $this->owner->RootFolder()->getRelativePath();
+        } else {
+            //use folder root as fallback for now
+            return $this->getFolderRoot();
+        }
+    }
 }
--- tests/RootFolderTest.php
+++ tests/RootFolderTest.php
@@ -6,134 +6,134 @@
 class RootFolderTest extends SapphireTest
 {
 
-	protected static $fixture_file = 'RootFolderTest.yml';
+    protected static $fixture_file = 'RootFolderTest.yml';
 
-	/**
-	 * Check if a folder is generated and saved when a page is saved.
-	 */
-	public function testCreateFolder()
-	{
-		$page = Page::create();
-		$this->assertEquals(0, $page->RootFolderID, 'a new Page should not have a folder yet');
+    /**
+     * Check if a folder is generated and saved when a page is saved.
+     */
+    public function testCreateFolder()
+    {
+        $page = Page::create();
+        $this->assertEquals(0, $page->RootFolderID, 'a new Page should not have a folder yet');
 
-		$page->Title = 'Create Page Test';
+        $page->Title = 'Create Page Test';
 
-		$page->write();
+        $page->write();
 
-		$this->assertNotEquals(0, $page->RootFolderID, 'a page should have a folder after saving');
+        $this->assertNotEquals(0, $page->RootFolderID, 'a page should have a folder after saving');
 
-		$folder = $page->RootFolder();
+        $folder = $page->RootFolder();
 
-		$this->assertEquals($page->URLSegment, $folder->Name, 'Page URLSegment and Folder Title should be the same');
-		$path = ASSETS_DIR . '/' . $root = Config::inst()->get('RootFolder', 'folder_root') . '/'
-				. $page->URLSegment . '/';
+        $this->assertEquals($page->URLSegment, $folder->Name, 'Page URLSegment and Folder Title should be the same');
+        $path = ASSETS_DIR . '/' . $root = Config::inst()->get('RootFolder', 'folder_root') . '/'
+                . $page->URLSegment . '/';
 
-		$this->assertEquals(
-			$path,
-			$folder->getRelativePath(),
-			'folder path should be assets/Articles/' . $page->URLSegment
-		);
-	}
+        $this->assertEquals(
+            $path,
+            $folder->getRelativePath(),
+            'folder path should be assets/Articles/' . $page->URLSegment
+        );
+    }
 
-	/**
-	 * Checks if the folder will be updated when saving a page and changing the URLSegment
-	 */
-	public function testUpdateFolder()
-	{
-		$page1 = $this->objFromFixture('Page', 'page1');
-		$folder = $page1->RootFolder();
+    /**
+     * Checks if the folder will be updated when saving a page and changing the URLSegment
+     */
+    public function testUpdateFolder()
+    {
+        $page1 = $this->objFromFixture('Page', 'page1');
+        $folder = $page1->RootFolder();
 
-		$this->assertEquals($page1->URLSegment, $folder->Name, 'Page URLSegment and Folder Title should be the same');
+        $this->assertEquals($page1->URLSegment, $folder->Name, 'Page URLSegment and Folder Title should be the same');
 
-		$page1->URLSegment = ('updatedpage');
-		$page1->write();
+        $page1->URLSegment = ('updatedpage');
+        $page1->write();
 
-		$folder = $page1->RootFolder(); //reload folder after saving
-		$this->assertEquals('updatedpage', $folder->Name, 'Folder name should be updated after saving a page');
-		$this->assertEquals(
-			$page1->URLSegment,
-			$folder->Name,
-			'Page URLSegment and Folder Title should be the same, even after updating'
-		);
-	}
+        $folder = $page1->RootFolder(); //reload folder after saving
+        $this->assertEquals('updatedpage', $folder->Name, 'Folder name should be updated after saving a page');
+        $this->assertEquals(
+            $page1->URLSegment,
+            $folder->Name,
+            'Page URLSegment and Folder Title should be the same, even after updating'
+        );
+    }
 
-	/**
-	 * Checks if no folder is created for ignored page types, e.g. VirtualPage or ErrorPage
-	 */
-	public function testIgnoredPageTypes()
-	{
-		$ignoredPageTypes = Config::inst()->get('RootFolder', 'ignored_classes');
+    /**
+     * Checks if no folder is created for ignored page types, e.g. VirtualPage or ErrorPage
+     */
+    public function testIgnoredPageTypes()
+    {
+        $ignoredPageTypes = Config::inst()->get('RootFolder', 'ignored_classes');
 
-		foreach ($ignoredPageTypes as $type) {
-			$page = $type::create();
+        foreach ($ignoredPageTypes as $type) {
+            $page = $type::create();
 
-			$page->write();
-			$this->assertEquals(
-				0,
-				$page->RootFolderID,
-				'Ignored page type ' . $type . ' should not have a RootFolderID'
-			);
-		}
-	}
+            $page->write();
+            $this->assertEquals(
+                0,
+                $page->RootFolderID,
+                'Ignored page type ' . $type . ' should not have a RootFolderID'
+            );
+        }
+    }
 
-	/**
-	 * Check if subpage's folder is a subfolder of parent page
-	 */
-	public function testHierarchy()
-	{
-		$parent = $this->objFromFixture('Page', 'parentpage');
-		$child = $this->objFromFixture('Page', 'subpage');
+    /**
+     * Check if subpage's folder is a subfolder of parent page
+     */
+    public function testHierarchy()
+    {
+        $parent = $this->objFromFixture('Page', 'parentpage');
+        $child = $this->objFromFixture('Page', 'subpage');
 
-		//test if fixtures are set up properly
-		$this->assertEquals($parent->ID, $child->ParentID, 'subpage should be a child of parentpage');
+        //test if fixtures are set up properly
+        $this->assertEquals($parent->ID, $child->ParentID, 'subpage should be a child of parentpage');
 
-		$this->assertEquals(
-			$parent->RootFolderID,
-			$child->RootFolder()->ParentID,
-			'rootfolder2 should be a child of rootfolder 1'
-		);
+        $this->assertEquals(
+            $parent->RootFolderID,
+            $child->RootFolder()->ParentID,
+            'rootfolder2 should be a child of rootfolder 1'
+        );
 
-		$newPage = Page::create();
-		$newPage->ParentID = $parent->ID;
-		$newPage->Title = 'Hierarchy Test';
-		$newPage->urlSegment = 'hierarchy-test';
-		$newPage->write();
+        $newPage = Page::create();
+        $newPage->ParentID = $parent->ID;
+        $newPage->Title = 'Hierarchy Test';
+        $newPage->urlSegment = 'hierarchy-test';
+        $newPage->write();
 
-		$this->assertEquals(
-			$parent->RootFolderID,
-			$newPage->RootFolder()->ParentID,
-			'new folder should be a child of page1 folder'
-		);
-	}
+        $this->assertEquals(
+            $parent->RootFolderID,
+            $newPage->RootFolder()->ParentID,
+            'new folder should be a child of page1 folder'
+        );
+    }
 
-	/**
-	 * Checks if getRootFolderName() works properly
-	 */
-	public function testGetRootFolderName()
-	{
-		$parent = $this->objFromFixture('Page', 'parentpage');
-		$child = $this->objFromFixture('Page', 'subpage');
+    /**
+     * Checks if getRootFolderName() works properly
+     */
+    public function testGetRootFolderName()
+    {
+        $parent = $this->objFromFixture('Page', 'parentpage');
+        $child = $this->objFromFixture('Page', 'subpage');
 
-		//test if fixtures are set up properly
-		$this->assertEquals($parent->ID, $child->ParentID, 'subpage should be a child of parentpage');
+        //test if fixtures are set up properly
+        $this->assertEquals($parent->ID, $child->ParentID, 'subpage should be a child of parentpage');
 
-		$this->assertStringEndsWith(
-			$child->RootFolder()->Name . '/',
-			$child->getRootFolderName(),
-			'FolderName should be at the end of getRootFolderName()'
-		);
+        $this->assertStringEndsWith(
+            $child->RootFolder()->Name . '/',
+            $child->getRootFolderName(),
+            'FolderName should be at the end of getRootFolderName()'
+        );
 
-		$root = Config::inst()->get('RootFolder', 'folder_root');
-		$this->assertStringStartsWith(
-			$root . '/' . $parent->RootFolder()->Name,
-			$child->getRootFolderName(),
-			'Parents FolderName should be at the beginning of getRootFolderName()'
-		);
+        $root = Config::inst()->get('RootFolder', 'folder_root');
+        $this->assertStringStartsWith(
+            $root . '/' . $parent->RootFolder()->Name,
+            $child->getRootFolderName(),
+            'Parents FolderName should be at the beginning of getRootFolderName()'
+        );
 
-		$this->assertStringStartsWith(
-			ASSETS_DIR,
-			$child->getRootFolderName(false),
-			'ASSETS_DIR should be at the beginning of getRootFolderName(false)'
-		);
-	}
-} 
+        $this->assertStringStartsWith(
+            ASSETS_DIR,
+            $child->getRootFolderName(false),
+            'ASSETS_DIR should be at the beginning of getRootFolderName(false)'
+        );
+    }
+}
